c4e37bdda581e316d98e50c96fc9c4fe334912a0
Yinon Levav <yinonlevav@gmail.com>
2024-01-14 16:52:52 +0200

    added index.ts

---
+++ index.ts
@@ -0,0 +1,9 @@
+import * as enums from "./enums";
+import * as formFieldInterfaces from "./formFieldsInterfaces";
+import * as interfaces from "./interfaces";
+
+export default {
+    ...enums,
+    ...formFieldInterfaces,
+    ...interfaces,
+}
