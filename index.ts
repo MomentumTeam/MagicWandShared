f7f49471123cbad4ef316e33fd21ec1c10c57a1a
Yinon Levav <yinonlevav@gmail.com>
2024-01-14 16:59:36 +0200

    index.ts fix exports

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
-import * as enums from "./enums";
-import * as formFieldInterfaces from "./formFieldsInterfaces";
-import * as interfaces from "./interfaces";
-
-export default {
-    ...enums,
-    ...formFieldInterfaces,
-    ...interfaces,
-}
+export * from "./enums";
+export * from "./formFieldsInterfaces";
+export * from "./interfaces";
