aa73e5883174a2259b47cc709deb250963fe256c
tamarperetz612 <tamarperetz612@gmail.com>
2024-01-30 11:04:15 +0200

    now using tsc

---
+++ permission/enums.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Permission = void 0;
+var Permission;
+(function (Permission) {
+    Permission["SUPER_ADMIN"] = "SUPER_ADMIN";
+    Permission["UNIT_ADMIN"] = "UNIT_ADMIN";
+    Permission["CORPS_EDITOR"] = "CORPS_EDITOR";
+    Permission["UNIT_EDITOR"] = "UNIT_EDITOR";
+})(Permission || (exports.Permission = Permission = {}));
