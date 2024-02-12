d7235e28da556de108c93b58124e0eadd7216630
Yinon Levav <yinonlevav@gmail.com>
2024-02-12 16:47:33 +0200

    compiled enums to js

---
+++ dist/form/interfaces.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FieldType = exports.ApproverStatus = exports.Status = void 0;
+const enums_1 = require("../field/enums");
+Object.defineProperty(exports, "FieldType", { enumerable: true, get: function () { return enums_1.FieldType; } });
+const enums_2 = require("./enums");
+Object.defineProperty(exports, "ApproverStatus", { enumerable: true, get: function () { return enums_2.ApproverStatus; } });
+Object.defineProperty(exports, "Status", { enumerable: true, get: function () { return enums_2.Status; } });
---
+++ dist/template/enums.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TemplateType = void 0;
+var TemplateType;
+(function (TemplateType) {
+    TemplateType["SECTION"] = "SECTION";
+    TemplateType["FIELD"] = "FIELD";
+})(TemplateType || (exports.TemplateType = TemplateType = {}));
